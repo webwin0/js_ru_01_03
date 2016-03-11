03d9930e757f4d829f73d373939b033de94549b0
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-11 16:16:29 +0200

    add styles for CSSTransitionGroup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import ArticleList from './ArticleList'
+import ArticleList from './components/ArticleList'
 import fixtures from './fixtures'
 import ReactDOM from 'react-dom'
 import React from 'react'
